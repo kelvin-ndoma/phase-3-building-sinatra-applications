016d07b07077a310fbd29fd3ed400d9444ef2e2f
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-02-27 15:18:50 +0300

    completed

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+      '<h2>Hello <em>World</em>!</h2>'
+    end
+    get '/' do
+        '<p>Kenya <em> yangu </em></p>'
+    end
+  
+  end
